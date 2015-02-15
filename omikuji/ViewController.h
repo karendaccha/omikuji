d170e2ccbf79b413e7680123f21fd4422c5bf8ce
KAREN <karen@KAREN-no-MacBook-Pro.local>
2015-02-15 16:37:00 +0900

    Initial Commit

---
+++ omikuji/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  omikuji
+//
+//  Created by KAREN on 2015/02/15.
+//  Copyright (c) 2015年 KAREN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
